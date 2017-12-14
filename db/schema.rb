fe4cb4e8a4dd010dba508adc202a7e056eff3dc7
ji hyuk kim <gskjhj@gmail.com>
2017-12-15 06:06:51 +0900

    hotfix a little

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171214062445) do
-=======
-ActiveRecord::Schema.define(version: 20171214085050) do
->>>>>>> feature/kwonb
 
   create_table "churns", force: :cascade do |t|
     t.string   "msno"
@@ -49,27 +45,6 @@ ActiveRecord::Schema.define(version: 20171214085050) do
     t.integer  "is_cancel"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-<<<<<<< HEAD
-=======
-    t.integer  "is_churn"
-    t.integer  "is_predictedChurn"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "role"
->>>>>>> feature/kwonb
   end
 
 end
