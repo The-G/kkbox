0e444b53d266196c13d1e8f57aee5f3c23d4b4cf
권보영 <kwonsooyoung@gwonsuyeong-ui-MacBook-Pro.local>
2017-12-14 12:57:59 +0900

    module dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,5 @@
 class AdminController < ApplicationController
 
-  before_action :transaction_is_churn, :transaction_this_month_expire_date, :transaction_new_transaction, :transaction_is_cancel
 
   def index
 
@@ -20,70 +19,50 @@ class AdminController < ApplicationController
   end
 
   def transaction_date
-    transaction_group('transaction_date')
+    transaction_group('transaction_date',"")
   end
 
   def transaction_membership_expire_date
-    transaction_group('membership_expire_date')
+    transaction_group('membership_expire_date',"")
   end
 
  def transaction_is_churn
-   transaction_this_group('is_predictedChurn')
+   transaction_group('is_predictedChurn',"count_is_churn")
  end
 
  def transaction_this_month_expire_date
-   transaction_this_group('membership_expire_date')
+   transaction_transaction_group('membership_expire_date','2017-03-1','2017-03-31',"count_expire_date")
  end
 
  def transaction_new_transaction
-   transaction_last_group('transaction_date')
+   transaction_transaction_group('transaction_date','2017-02-1','2017-02-31',"count_new_transaction")
  end
 
  def  transaction_is_cancel
-   transaction_last_group('is_cancel')
+   transaction_is_cancel_group('is_cancel')
  end
 
 private
-  def transaction_group(group)
+  def transaction_group(group, key)
     transaction=Transaction.group(group).count
     count=[]
     transaction.each do |k, v|
-      count.push({"date"=> k, "number"=>v, "number2"=>rand(v-10..v+10)}, "")
+      count.push({"date"=> k, "number"=>v, "number2"=>rand(v-10..v+10), key => transaction[1]})
     end
     render :json => count
   end
 
-  def transaction_this_group(group)
-    transaction = Transaction.group(group).count
+  def transaction_transaction_group(group, begin_day, end_day, key)
+    transaction = Transaction.where(group+' between ? and ?', begin_day, end_day).group(group).count()
     values=[]
-    count_is_churn = 0
-    count_membership_expire_date = 0
-    transaction.each do |k|
-      if is_predictedChurn==1
-        count_is_churn += 1
-      end
-      if transaction.membership_expire_date.strftime("%Y") == Time.now.strftime("%Y") && transaction.membership_expire_date.strftime("%m") == Time.now.strftime("%m")
-        count_membership_expire_date += 1
-      end
-      values.push({"count_is_churn"=>count_is_churn, "count_membership_expire_date"=>count_membership_expire_date})
-    end
+    values.push({key=>transaction.count})
     render :json => values
   end
 
-  def transaction_last_group(group)
-    transaction = Transaction.group(group).count
+  def transaction_is_cancel_group(group)
+    transaction = Transaction.where('transaction_date between ? and ? and is_cancel = ?', '2017-02-1', '2017-02-31','1').group(group).count()
     values=[]
-    count_new_transaction = 0
-    count_is_cancel = 0
-    transaction.each do |k|
-      if transaction.membership_expire_date.strftime("%Y") == Time.now.strftime("%Y") && transaction.membership_expire_date.strftime("%m") == Time.now.strftime("%m")-1 && transaction.is_cancel ==1
-        count_is_cancel += 1
-      end
-      if transaction.transaction_date.strftime("%Y") == Time.now.strftime("%Y") && transaction.transaction_date.strftime("%m") == Time.now.strftime("%m")-1
-        count_new_transaction += 1
-      end
-      values.push({"count_is_cancel"=>count_is_cancel, "count_new_transaction"=>count_new_transaction})
-    end
+    values.push({"count_is_cancel"=>transaction.count})
     render :json => values
   end
 
